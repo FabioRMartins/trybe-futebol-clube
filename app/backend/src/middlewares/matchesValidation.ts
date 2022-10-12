228bf5d718a8eda9d15dfeb87f3575fc10026ce7
FabioMartins <fabbio1000@gmail.com>
2022-10-12 00:45:08 -0300

    Feat: Criando validação para não ter partidas com times inexistentes

--- app/backend/src/middlewares/matchesValidation.ts
+++ app/backend/src/middlewares/matchesValidation.ts
@@ -5,8 +5,10 @@ export default class MatchesValidation {
     const { homeTeam, awayTeam } = req.body;
     if (homeTeam === awayTeam) {
       return res.status(401).json({
-        message: 'It is not possible to create a match with two equal teams',
-      });
+        message: 'It is not possible to create a match with two equal teams' });
+    }
+    if (homeTeam > 16 || awayTeam > 16) {
+      return res.status(404).json({ message: 'There is no team with such id!' });
     }
     next();
   };
